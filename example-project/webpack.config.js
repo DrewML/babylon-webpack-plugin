90e7e9648f3c923dde9a7c3081e8452fe64a400f
Andrew Levine <theandrewtizer@gmail.com>
2018-01-03 22:41:51 -0600

    Add babel-loader to the example project

--- example-project/webpack.config.js
+++ example-project/webpack.config.js
@@ -10,5 +10,20 @@ module.exports = {
         path: join(__dirname, 'dist'),
         filename: '[name].js'
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: BabylonWebpackPlugin.transformBabelOpts({
+                        parserOpts: {
+                            plugins: ['dynamicImport']
+                        }
+                    })
+                }
+            }
+        ]
+    },
     plugins: [new BabylonWebpackPlugin(require.resolve('webpack'))]
 };
